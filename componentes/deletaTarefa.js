91eff9127e08d046a77b6186774a07223f8af1a6
anderleand <andersonleandro.desouza@gmail.com>
2021-03-22 18:58:19 -0300

    Update deletaTarefa.js

--- componentes/deletaTarefa.js
+++ componentes/deletaTarefa.js
@@ -1,12 +1,12 @@
-const deletarTarefa = (atualiza, id) => { 
+const deletarTarefa = (atualiza, id) => {
     const index = id
     const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))
-    localStorage.setItem('tarefas', JSON.stringify(tarefasCadastradas))
     tarefasCadastradas.splice(index, 1)
+    localStorage.setItem('tarefas', JSON.stringify(tarefasCadastradas))
     atualiza()
 }
 
-const BotaoDeleta = (atualiza, id) => { 
+const BotaoDeleta = (atualiza, id) => {
     const botaoDeleta = document.createElement('button')
 
     botaoDeleta.innerText = 'Deletar'
